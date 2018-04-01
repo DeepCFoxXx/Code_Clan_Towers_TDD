dec19fb391517432a72b3d8dc54c75acbd0e7c2d
DeepCFoxXx <learncode23@gmail.com>
2018-04-01 14:32:06 +0100

    hotel name passing

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private String name;
+    private ArrayList<Bedroom> bedrooms;
+    private DiningRoom  diningRoom;
+    private ConferenceRoom conferenceRoom;
+
+    public Hotel(String name) {
+        this.name = name;
+        bedrooms = new ArrayList<Bedroom>();
+    }
+
+
+    public String getName() {
+        return this.name;
+    }
 }
--- src/test/java/TestHotel.java
+++ src/test/java/TestHotel.java
@@ -1,2 +1,36 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestHotel {
+
+    Hotel hotel;
+    Bedroom singleRoom;
+    Bedroom doubleRoom;
+    Bedroom twinRoom;
+    ConferenceRoom conferenceRoom;
+    DiningRoom diningRoom;
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
+
+    @Before
+    public void before() {
+        hotel = new Hotel("Galaxy Towers");
+        singleRoom = new Bedroom(101, BedroomType.SINGLE, 25.00);
+        doubleRoom = new Bedroom(102, BedroomType.DOUBLE, 50.00);
+        twinRoom = new Bedroom(103, BedroomType.TWIN, 60.00);
+        conferenceRoom = new ConferenceRoom("Coruscant", 2, 1000.00);
+        diningRoom = new DiningRoom("Mos Eisley Cantina", 2);
+        guest1 = new Guest("Leia Organa");
+        guest2 = new Guest("Mara Jade");
+        guest3 = new Guest("Rey Solo");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Galaxy Towers", hotel.getName());
+    }
+
 }
