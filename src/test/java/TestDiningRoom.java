d2f89afb4a16f11615e9397be60368b7ec1b0e08
DeepCFoxXx <learncode23@gmail.com>
2018-04-01 13:24:43 +0100

    dining room capacity passing

--- src/test/java/TestDiningRoom.java
+++ src/test/java/TestDiningRoom.java
@@ -9,12 +9,17 @@ public class TestDiningRoom {
 
     @Before
     public void before() {
-        room = new DiningRoom("Mos Eisley Cantina", 50);
+        room = new DiningRoom("Mos Eisley Cantina", 45);
     }
 
     @Test
     public void hasName() {
         assertEquals("Mos Eisley Cantina", room.getName());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(45, room.getCapacity());
     }
 
 }
